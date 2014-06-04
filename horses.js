a70c0e888fa504e7c283fde146de7e26d67a1836
Julien Brun <julien.brun@hotmail.co.uk>
2014-06-04 13:00:09 +0100

    Update horses.js

--- horses.js
+++ horses.js
@@ -6,22 +6,42 @@ var Horses = function (outerHorsesElements, options) {
     this.innerhorses = $(outerHorsesElements).getElements('horserider').getElements('.horse');
     this.numberOfHorses = $(outerHorsesElements).getElement('.horserider').getElements('.horse').length;
     this.horseWidth = $(outerHorsesElements).getElement('.horserider').getElement('.horse').getStyle('width').replace('px', '');
+    this.horseHeight = $(outerHorsesElements).getElement('.horserider').getElement('.horse').getStyle('height').replace('px', '');
     this.pPosition = 1
+    if (typeof this.options.vertical == 'undefined') {
+        this.options.vertical == false;
+    }
 
-    $(outerHorsesElements).getElement('.horserider').setStyle('width', this.numberOfHorses * 100 + '%');
 
     if (typeof options.paneWidth != undefined) {
-        $(outerHorsesElements).setStyle('width', (options.paneWidth * this.horseWidth) + 'px')
+        $(outerHorsesElements).setStyle('width', (options.paneWidth * this.horseWidth) + 'px');
     } else {
-        $(outerHorsesElements).setStyle('width', this.horseWidth + 'px')
+        $(outerHorsesElements).setStyle('width', this.horseWidth + 'px');
         this.options.paneWidth = 1;
     }
 
+    this.updateDims = function () {
+        if (this.options.vertical == true) {
+            $(outerHorsesElements).setStyle('width', this.horseWidth + 'px');
+            $(outerHorsesElements).setStyle('height', (options.paneWidth * this.horseHeight) + 'px');
+            $(outerHorsesElements).getElement('.horserider').setStyle('height', this.numberOfHorses * 100 + '%');
+            $(outerHorsesElements).getElement('.horserider').setStyle('width', '100%');
+        } else {
+            $(outerHorsesElements).getElement('.horserider').setStyle('width', this.numberOfHorses * 100 + '%');
+            $(outerHorsesElements).getElement('.horserider').setStyle('height', '100%');
+        }
+    }
+    this.updateDims();
     this.jumpTo = function (position) {
         //$(outerHorsesElements).getElement('.horserider').setStyle('display', 'none');
 
         var hPos = this.horseWidth - (position * this.horseWidth);
-        $(outerHorsesElements).getElement('.horserider').setStyle('left', hPos + 'px');
+        var hPosV = this.horseHeight - (position * this.horseHeight);
+        if (this.options.vertical == true) {
+            $(outerHorsesElements).getElement('.horserider').setStyle('top', hPosV + 'px');
+        } else {
+            $(outerHorsesElements).getElement('.horserider').setStyle('left', hPos + 'px');
+        }
         this.pPosition = position;
     };
 
@@ -46,7 +66,9 @@ var Horses = function (outerHorsesElements, options) {
     }
 
     this.inject = function (elToInject) {
-
+        elToInject.inject($(outerHorsesElements).getElement('.horserider'));
+        this.numberOfHorses++;
+        this.updateDims();
     }
 
 
